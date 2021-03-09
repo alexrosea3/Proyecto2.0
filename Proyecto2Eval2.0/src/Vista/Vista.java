1ed136501483a3dc214aa418c0adfbb2165de7f7
alexrosea3 <77998606+alexrosea3@users.noreply.github.com>
2021-03-09 10:51:26 +0100

    Update Vista.java

--- Proyecto2Eval2.0/src/Vista/Vista.java
+++ Proyecto2Eval2.0/src/Vista/Vista.java
@@ -12,7 +12,7 @@ import Modelo.Modelo;
 public class Vista {
 
 	JTextField cajatext = new JTextField(30);
-	ArrayList<JButton> btn = new ArrayList();
+	ArrayList<JButton> botones = new ArrayList();
 	JPanel panel = new JPanel();
 	JFrame frame = new JFrame();
 	Modelo modelo = new Modelo();
@@ -20,7 +20,8 @@ public class Vista {
 	public Vista() {
 
 		for (int i = 0; i < 3; i++) {
-			panel.add(new JButton(modelo.getTxtbtn().get(i)));
+			botones.add(new JButton(modelo.getTxtbtn().get(i)));
+			panel.add(botones.get(i));
 		}
 		panel.add(cajatext);
 		frame.setSize(400, 400);
@@ -30,4 +31,45 @@ public class Vista {
 		frame.setVisible(true);
 
 	}
+
+	public JTextField getCajatext() {
+		return cajatext;
+	}
+
+	public void setCajatext(JTextField cajatext) {
+		this.cajatext = cajatext;
+	}
+
+	public ArrayList<JButton> getBotones() {
+		return botones;
+	}
+
+	public void setBotones(ArrayList<JButton> botones) {
+		this.botones = botones;
+	}
+
+	public JPanel getPanel() {
+		return panel;
+	}
+
+	public void setPanel(JPanel panel) {
+		this.panel = panel;
+	}
+
+	public JFrame getFrame() {
+		return frame;
+	}
+
+	public void setFrame(JFrame frame) {
+		this.frame = frame;
+	}
+
+	public Modelo getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(Modelo modelo) {
+		this.modelo = modelo;
+	}
+
 }
