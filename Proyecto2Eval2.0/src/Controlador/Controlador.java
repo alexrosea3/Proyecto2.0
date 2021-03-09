276562f14d9fc5b5f3011edc54d78f614e66c6b0
jmvelasc0 <80314621+jmvelasc0@users.noreply.github.com>
2021-03-09 10:40:29 +0100

    Update Controlador.java

--- Proyecto2Eval2.0/src/Controlador/Controlador.java
+++ Proyecto2Eval2.0/src/Controlador/Controlador.java
@@ -1,5 +1,34 @@
 package Controlador;
 
+import java.util.ArrayList;
+
+import Modelo.Ingles;
+import Modelo.Español;
+import Modelo.Frances;
+
 public class Controlador {
+	private Ingles ingles;
+	private Español español;
+	private Fances frances;
+	private ArrayList<Palabras> palabras = new ArrayList<>();
+	
+	public Controlador(){
+		ingles = new Ingles();
+		español = new Español();
+		frances = new Frances();
+		rellenarPalabras();
+	}
+	
+	
+	// METODOS
+	
+
+	private void rellenarPalabras() {
+		for(int j = 0; j < getIdiomas().size(); j++){
+		for(int i = 0; i < getIdiomas(j).size(); i++){
+			Idiomas[i] = new (getIdiomas.get(i));
+		}
+	}
+	}
 
 }
